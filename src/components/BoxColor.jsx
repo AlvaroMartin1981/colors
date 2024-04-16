c4f7bac53d8d159eb80f4ac69b78a4f7324f7e84
titopope@hotmail.com <titopope@hotmail.com>
2024-04-16 21:54:26 +0200

    despues de clase

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,8 +1,39 @@
-function BoxColor ({color, value}) {
+import { useRef, useState, useEffect } from "react"
+
+function boxColor({color, value}) {
+  const [classList, setClassList] = useState('')
+  const boxRef = useRef(null)
+
+  const boxCss = {
+    backgroundColor: classList.value == `box ${value}` ? color : null,
+    border: `1px solid ${color}`,
+    color: color
+  }
+
+  useEffect(() =>{
+    setClassList(boxRef.current.classList)
+  }, [])
+
+  return(
+  <>
+  <div 
+  ref={boxRef} 
+  className={`box ${color}`}
+  style={boxCss}
+  >
+  {color === value ? <h2 style={{color: 'black'}}>Yes, I´m {value} color</h2> : <h2>I´m not {value} color</h2>}  
+
+  </div>
+  </>
+  )
+}
+export default boxColor
+
+/*function BoxColor ({color, value}) {
   return (
     <div>
       <span className={`box ${color}`} style={{color:color}}>{value}</span>
     </div>
   )
 }
-export default BoxColor;  
+export default BoxColor;  */
--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,4 +1,40 @@
-import { useEffect, useState, useRef } from "react";
+import { useRef, useState } from "react"
+import BoxColor from './BoxColor'
+
+function MyForm() {
+  const inputRef = useRef(null);
+  const [value, setValue] = useState('')
+  const colors =['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive']
+  
+  
+
+  return(
+    <>
+    <input
+      type="text"
+      placeholder="choose a color"
+      value={value}
+      ref={inputRef}
+      onChange={() => setValue(inputRef.current.value)}
+    />
+    <div className="container">
+      {colors.map((color, index) => {
+        return (<BoxColor key={index} color={color} value={value}/>)
+      })}
+    </div>
+
+    
+    </>
+  )
+}
+export default MyForm
+
+
+
+
+
+
+/*import { useEffect, useState, useRef } from "react";
 import '../App.css'
 import BoxColor from "./BoxColor";
 const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
@@ -37,4 +73,4 @@ function MyForm() {
   );
 }
 
-export default MyForm;
+export default MyForm;*/
